dc2de378566e8723d459e639b84bff824a33a9ad
Liliia Bilous <liliyabilous.email@gmail.com>
2024-10-19 14:48:38 +0300

    added animation

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
     const sidebar = document.querySelector(".aside");
+    const content = document.querySelector(".main");
     const menuToggle = document.querySelector(".menu-toggle");
     let isExpanded = false;
     
@@ -10,9 +11,27 @@ document.addEventListener("DOMContentLoaded", function () {
 
     function updateSidebar() {
         if (isExpanded) {
+            content.classList.add("collapce");
             sidebar.classList.add("collapce");
         } else {
             sidebar.classList.remove("collapce");
+            content.classList.remove("collapce");
         }
     }
-});
+    gsap.registerPlugin(ScrollTrigger);
+
+    const rows = document.querySelectorAll('.table__row');
+    rows.forEach((row, index) => {
+        gsap.from(row, {
+          opacity: 0,
+          y: 50, 
+          duration: 0.6,
+          delay: index * 0.1, 
+          scrollTrigger: {
+            trigger: row, 
+            start: 'top 98%', 
+            toggleActions: 'play none none none',
+          }
+        });
+})
+})
